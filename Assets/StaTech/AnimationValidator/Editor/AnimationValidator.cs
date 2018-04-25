dc38e60dd4b99efdd142489d81fec49da8098633
toru_sugitani <skj.pass.2011@gmail.com>
2018-04-26 02:30:16 +0900

    first commit

---
+++ Assets/StaTech/AnimationValidator/Editor/AnimationValidator.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Animations;
+using UnityEngine;
+
+namespace StaTech.AnimationValidator
+{
+    public static class AnimationValidator
+    {
+        private static Animator _animator;
+
+        private static readonly string[] CurveNames =
+        {
+            "m_PositionCurves",
+            "m_ScaleCurves",
+            "m_FloatCurves",
+            "m_PPtrCurves",
+            "m_EditorCurves",
+            "m_EulerEditorCurves"
+        };
+
+        private static readonly string PathPropName      = "path";
+        private static readonly string AttributePropName = "attribute";
+
+        private static List <ClipValidationContainer> _clipValidations;
+
+        public static List <ClipValidationContainer> ValidateAnimation()
+        {
+            if (Selection.activeGameObject == null) { return null; }
+
+            var selectedTransform = Selection.activeGameObject.transform;
+            // アニメーションクリップを取り出すAnimator
+            _animator = selectedTransform.GetComponent <Animator>();
+
+            if (_animator == null)
+            {
+                // 警告window出す
+                EditorUtility.DisplayDialog("エラー", "選択したオブジェクトにAnimatorがついていません", "閉じる");
+
+                return null;
+            }
+
+            var runTimeAnimatorController = _animator.runtimeAnimatorController;
+            var animationController       = runTimeAnimatorController as AnimatorController;
+
+            if (animationController == null)
+            {
+                // 警告window出す
+                EditorUtility.DisplayDialog("エラー", "AnimatorにAnimationControllerが設定されていません", "閉じる");
+
+                return null;
+            }
+
+            if (_clipValidations == null) {
+                _clipValidations = new List <ClipValidationContainer>();
+            }
+            else {
+                _clipValidations.Clear();
+            }
+
+            // 全てのレイヤーを取り出す
+            for (var i = 0; i < animationController.layers.Length; i++)
+            {
+                var layer        = animationController.layers[i];
+                var stateMachine = layer.stateMachine;
+                // 全てのステートを取り出す
+                for (var j = 0; j < stateMachine.states.Length; j++)
+                {
+                    var state = stateMachine.states[j];
+                    var clip  = state.state.motion as AnimationClip;
+                    if (!clip) { continue; }
+                    var validationData = FindLostAnimations(clip, selectedTransform);
+                    _clipValidations.Add(validationData);
+                }
+            }
+
+            return _clipValidations;
+        }
+
+        public static void ExecuteUnitRecovery(GameObject selected, ClipValidationContainer container,
+                                               Action <string, float> onProgressChagned)
+        {
+            var childObjectsNames = GetChildObjectNames(selected);
+            Recovery(container, ref childObjectsNames, onProgressChagned);
+            EditorUtility.ClearProgressBar();
+        }
+
+        public static void ExecuteAllRecovery(GameObject selected, List <ClipValidationContainer> containers,
+                                              Action <string, float> onProgressChanged)
+        {
+            var childObjectsNames = GetChildObjectNames(selected);
+            var targetCount       = containers.Count;
+            for (var i = 0; i < containers.Count; i++)
+            {
+                var container = containers[i];
+                Recovery(container, ref childObjectsNames);
+                if (onProgressChanged != null) { onProgressChanged.Invoke(container.ClipName, (float) i / targetCount); }
+            }
+
+            AssetDatabase.SaveAssets();
+            EditorUtility.ClearProgressBar();
+        }
+
+        private static ClipValidationContainer FindLostAnimations(AnimationClip clip, Transform root)
+        {
+            var lostAnimations = new List <LostProperty>();
+            var serializedClip = new SerializedObject(clip);
+            // clipはいくつかのカーブをもっている
+            foreach (var curveName in CurveNames)
+            {
+                var curves     = serializedClip.FindProperty(curveName);
+                var curveCount = curves.arraySize;
+                for (var i = 0; i < curveCount; i++)
+                {
+                    var curve             = curves.GetArrayElementAtIndex(i);
+                    var pathProperty      = curve.FindPropertyRelative(PathPropName);
+                    var attributeProperty = curve.FindPropertyRelative(AttributePropName);
+                    var attribute         = attributeProperty != null ? attributeProperty.stringValue : "Position";
+                    // ルートの直下からの相対パスが入ってる
+                    var path   = pathProperty.stringValue;
+                    var result = root.Find(path);
+                    if (result == null)
+                    {
+                        var lost = new LostProperty
+                        {
+                            ObjectName         = GetObjectName(path),
+                            PropPath           = path,
+                            AttributeName      = attribute,
+                            SerializedProperty = pathProperty,
+                            SerializedClip     = serializedClip,
+                            State              = FixState.Lost
+                        };
+                        lostAnimations.Add(lost);
+                    }
+                }
+            }
+
+            var clipLost = new ClipValidationContainer(lostAnimations, clip);
+
+            return clipLost;
+        }
+
+        private static void Recovery(ClipValidationContainer validationContainer, ref List <PathModel> paths,
+                                     Action <string, float> onUnitProgressChanged=null)
+        {
+            for (var i = 0; i < validationContainer.LostProperties.Count; i++)
+            {
+                var anim            = validationContainer.LostProperties[i];
+                var pathInHierarchy = paths.Where(path => path.ObjectName == anim.ObjectName).ToList();
+                var hierarchyCount  = pathInHierarchy.Count;
+
+                if (onUnitProgressChanged != null) {
+                    onUnitProgressChanged.Invoke(anim.ObjectName, (float) i / validationContainer.LostProperties.Count);
+                }
+
+                if (hierarchyCount == 0)
+                {
+                    //同名のobjectNameが存在しない
+                    anim.State = FixState.ErrorNoSameName;
+                    continue;
+                }
+
+                if (hierarchyCount >= 2)
+                {
+                    // 複数のobjectが存在するから修正出来ない
+                    anim.State = FixState.ErrorDuplicate;
+                    continue;
+                }
+
+
+                // 修復処理
+                var correctPath = pathInHierarchy.First().RelativePath;
+                Debug.Log(anim.PropPath + "を" + correctPath + "に修正");
+                anim.State                          = FixState.Fixed;
+                anim.SerializedProperty.stringValue = correctPath;
+                anim.SerializedClip.ApplyModifiedProperties();
+            }
+        }
+
+        public static string GetObjectName(string path)
+        {
+            if (string.IsNullOrEmpty(path)) { return ""; }
+
+            var separated = path.Split('/');
+
+            return separated[separated.Length - 1];
+        }
+
+        private static List <PathModel> GetChildObjectNames(GameObject root)
+        {
+            var children = GetAllObjects(root);
+
+            return children.Select(obj =>
+            {
+                // なんか進捗とか出す
+                return new PathModel(obj, root.name);
+            }).ToList();
+        }
+
+        public static List <GameObject> GetAllObjects(GameObject obj)
+        {
+            var allChildren = new List <GameObject>();
+            GetChildren(obj, ref allChildren);
+
+            return allChildren;
+        }
+
+        public static void GetChildren(GameObject obj, ref List <GameObject> allChildren)
+        {
+            var children = obj.GetComponentInChildren <Transform>();
+            // 子要素がいなければ終了
+            if (children.childCount == 0) { return; }
+            foreach (Transform ob in children)
+            {
+                allChildren.Add(ob.gameObject);
+                GetChildren(ob.gameObject, ref allChildren);
+            }
+        }
+
+        public static string ParentRelativePath(Transform t, string path, string rootName)
+        {
+            var parent = t.parent;
+            if (parent == null)
+            {
+                if (t.name != rootName) { Debug.LogError("不正な階層指定してます" + rootName); }
+
+                return path;
+            }
+
+            if (parent.name == rootName) { return path; }
+            path = parent.name + "/" + path;
+
+            return ParentRelativePath(parent, path, rootName);
+        }
+
+        private class PathModel
+        {
+            public readonly string ObjectName;
+            public readonly string RelativePath;
+
+            public PathModel(GameObject go, string rootName)
+            {
+                ObjectName   = go.name;
+                RelativePath = ParentRelativePath(go.transform, ObjectName, rootName);
+            }
+        }
+    }
+
+    public class ClipValidationContainer
+    {
+        public ClipValidationContainer(List <LostProperty> lostAnims, AnimationClip clip)
+        {
+            LostProperties = lostAnims;
+            ClipName       = clip.name;
+        }
+
+        public string ClipName
+        {
+            get; set;
+        }
+
+        public bool HasNoError
+        {
+            get { return LostProperties.Count == 0 || LostProperties.All(p => p.State == FixState.Fixed); }
+        }
+
+        public List <LostProperty> LostProperties
+        {
+            get; private set;
+        }
+    }
+
+    public class LostProperty
+    {
+        public string AttributeName;
+
+        /// <summary>
+        ///     オブジェクトの名前
+        ///     一意である必要がある
+        /// </summary>
+        public string ObjectName;
+
+        /// <summary>
+        ///     アニメーション内のプロパティのパス
+        /// </summary>
+        public string PropPath;
+
+        public SerializedObject SerializedClip;
+
+        public SerializedProperty SerializedProperty;
+
+        /// <summary>
+        ///     修復済みフラグ
+        /// </summary>
+        public FixState State;
+    }
+
+    public enum FixState
+    {
+        None,
+        Lost,
+        ErrorNoSameName,
+        ErrorDuplicate,
+        Fixed
+    }
+}
---
+++ Assets/StaTech/AnimationValidator/Editor/AnimationValidatorView.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace StaTech.AnimationValidator
+{
+    public class AnimationValidatorView : EditorWindow
+    {
+        private Texture2D _successIcon;
+        private Texture2D SuccessIcon
+        {
+            get
+            {
+                if (_successIcon == null)
+                {
+                    _successIcon = GetBuiltInIcon("testpassed.png");
+                }
+
+                return _successIcon;
+            }
+        }
+
+        private Texture2D _errorIcon;
+        private Texture2D ErrorIcon
+        {
+            get
+            {
+                if (_errorIcon == null)
+                {
+                    _errorIcon = GetBuiltInIcon("testfailed.png");
+                }
+
+                return _errorIcon;
+            }
+        }
+
+        private const float DetailSpace  = 30f;
+        private const float RightPadding = 30f;
+
+        private static List <ClipValidationContainer> _clipValidations;
+        private static GameObject _selectedObject;
+        private static AnimationValidatorView _window;
+        private static Vector2 _scrollPos;
+
+        [MenuItem("GameObject/アニメーションクリップ修正", false, -1)]
+        public static void Open()
+        {
+            _clipValidations = AnimationValidator.ValidateAnimation();
+            if (_clipValidations == null)
+            {
+                return;
+            }
+            _window              = GetWindow <AnimationValidatorView>();
+            _window.titleContent = new GUIContent("anim修正");
+            _selectedObject      = Selection.activeGameObject;
+        }
+
+        private void OnGUI()
+        {
+            if (_clipValidations == null || _clipValidations.Count == 0)
+            {
+                GUILayout.Label("AnimationClipがありません");
+
+                return;
+            }
+
+            _scrollPos = EditorGUILayout.BeginScrollView(_scrollPos, true, true);
+            GUILayout.Label("=====================================");
+            GUILayout.Label("Animationでmissingになっているものを表示します");
+            GUILayout.Label("オブジェクトの名前が他と被っていなれば自動で修正します");
+            GUILayout.Label("=====================================");
+
+            if (_clipValidations.Count > 1)
+            {
+                foreach (var result in _clipValidations)
+                {
+                    if (!result.HasNoError)
+                    {
+                        DrawButton("全部まとめて修正", () =>
+                        {
+                            AnimationValidator.ExecuteAllRecovery(_selectedObject, _clipValidations, ShowProgress);
+                            _window.Repaint();
+                            EditorApplication.RepaintAnimationWindow();
+                        });
+                        break;
+                    }
+                }
+            }
+
+            foreach (var result in _clipValidations)
+            {
+                DrawResult(result);
+            }
+            DrawButton("閉じる", () => _window.Close());
+            EditorGUILayout.EndScrollView();
+        }
+
+        private void DrawResult(ClipValidationContainer result)
+        {
+            if (result.HasNoError)
+            {
+                using (new EditorGUILayout.VerticalScope("box", GUILayout.Width(position.width - RightPadding)))
+                {
+                    DrawOnSuccess(result);
+                }
+            }
+            else
+            {
+                using (new EditorGUILayout.VerticalScope("box", GUILayout.Width(position.width - RightPadding)))
+                {
+                    DrawOnError(result);
+                }
+            }
+        }
+
+        private void DrawOnSuccess(ClipValidationContainer result)
+        {
+            DrawIconAndLabel(SuccessIcon, result.ClipName, "→正常です");
+            DrawDetail(result.LostProperties);
+        }
+
+        private void DrawOnError(ClipValidationContainer result)
+        {
+            DrawIconAndLabel(ErrorIcon, result.ClipName);
+            DrawDetail(result.LostProperties);
+            DrawButton("自動修正", () =>
+            {
+                AnimationValidator.ExecuteUnitRecovery(_selectedObject, result, ShowProgress);
+                _window.Repaint();
+                EditorApplication.RepaintAnimationWindow();
+            });
+        }
+
+        private void ShowProgress(string content, float progress)
+        {
+            EditorUtility.DisplayProgressBar("実行中", content, progress);
+        }
+
+        private void DrawDetail(List <LostProperty> Props)
+        {
+            using (new GUILayout.VerticalScope())
+            {
+                foreach (var prop in Props)
+                {
+                    var icon = prop.State == FixState.Fixed ? SuccessIcon : ErrorIcon;
+                    DrawSpaceAndText(DetailSpace, prop.PropPath + " : " + prop.AttributeName, icon, prop.State);
+                }
+            }
+        }
+
+        private void DrawIconAndLabel(Texture image, string content, string detail="")
+        {
+            using (new GUILayout.HorizontalScope())
+            {
+                GUILayout.Label(image, GUILayout.Height(18f), GUILayout.Width(20f));
+                GUILayout.Label(content);
+                if (!string.IsNullOrEmpty(detail))
+                {
+                    GUIStyle s = new GUIStyle(EditorStyles.label)
+                    {
+                        normal = { textColor = Color.green }
+                    };
+                    GUILayout.Label(detail, s);
+                    GUILayout.FlexibleSpace();
+                }
+            }
+        }
+
+        private void DrawSpaceAndText(float space, string content, Texture headIcon, FixState state=FixState.None)
+        {
+            using (new GUILayout.HorizontalScope())
+            {
+                GUILayout.Space(space);
+                if (headIcon)
+                {
+                    GUILayout.Label(headIcon, GUILayout.Height(18f), GUILayout.Width(20f));
+                }
+                GUILayout.Label(content);
+                var detail = GetDetail(state);
+                if (!string.IsNullOrEmpty(detail))
+                {
+                    // 黄色文字で足す
+                    var color = GetColor(state);
+                    if (color != Color.white)
+                    {
+                        GUIStyle s = new GUIStyle(EditorStyles.label)
+                        {
+                            normal = { textColor = color }
+                        };
+                        GUILayout.Label(detail, s);
+                        GUILayout.FlexibleSpace();
+                    }
+                }
+            }
+        }
+
+        private static Color GetColor(FixState state)
+        {
+            switch (state)
+            {
+                case FixState.ErrorDuplicate:
+                case FixState.ErrorNoSameName:
+
+                    return Color.yellow;
+                case FixState.Fixed:
+
+                    return Color.green;
+                default:
+
+                    return Color.white;
+            }
+        }
+
+        private static string GetDetail(FixState state)
+        {
+            switch (state)
+            {
+                case FixState.ErrorDuplicate:
+
+                    return "→同じ名前のオブジェクトが子階層上に複数あります";
+                case FixState.ErrorNoSameName:
+
+                    return "→同じ名前のオブジェクトが見つかりませんでした";
+                case FixState.Lost:
+
+                    return " ";
+                case FixState.Fixed:
+
+                    return "→Animationのパスを変更しました";
+
+                default:
+
+                    return "";
+            }
+        }
+
+        private void DrawButton(string buttonName, System.Action callback=null)
+        {
+            if (GUILayout.Button(buttonName, GUILayout.Width(200f)))
+            {
+                if (callback != null)
+                {
+                    callback.Invoke();
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            if (_clipValidations != null)
+            {
+                _clipValidations.Clear();
+                _clipValidations = null;
+            }
+        }
+
+        private static Texture2D GetBuiltInIcon(string name)
+        {
+            System.Reflection.MethodInfo mi = typeof(EditorGUIUtility).GetMethod("IconContent", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public, null, new System.Type[] { typeof(string) }, null);
+            if (mi == null) { mi = typeof(EditorGUIUtility).GetMethod("IconContent", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.NonPublic, null, new System.Type[] { typeof(string) }, null); }
+
+            return (Texture2D) ((GUIContent) mi.Invoke(null, new object[] { name })).image;
+        }
+    }
+}
